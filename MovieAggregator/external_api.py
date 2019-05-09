a830415d5d650ce150f677792bf2633263f911b5
Dawid <dawid.deby@gmail.com>
2019-05-09 17:32:48 +0200

    logger

--- MovieAggregator/external_api.py
+++ MovieAggregator/external_api.py
@@ -1,8 +1,11 @@
+import logging
+
 import requests
 from requests.exceptions import ConnectionError
 
 from .exceptions import ExternalApiConnectionError, MovieDoesNotExists
 
+logger = logging.getLogger(__name__)
 
 OMDB_API_KEY = '8f6b5665'
 
@@ -26,8 +29,8 @@ class Api:
         url = API_URL + '&t=' + title
         try:
             response = requests.get(url)
-        except ConnectionError:
-            # log info here
+        except ConnectionError as e:
+            logger.error(e)
             raise ExternalApiConnectionError('ConnectionError with fetch data')
         else:
             data = map_json(response.json())
