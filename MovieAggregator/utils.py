df8fc63ad54b74d27f7d4efe69cb0f9113eb86a7
Dawid <dawid.deby@gmail.com>
2019-07-08 23:38:10 +0200

    add utils.py

---
+++ MovieAggregator/utils.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+
+def prepare_date(date):
+    try:
+        return datetime.strptime(date, '%Y-%m-%dT%H:%M:%S') if date else None
+    except ValueError:  # invalid dateformat
+        return None
